820bec5565212a214c3a4d586021dbab8b017364
김승기 <skkim0832@naver.com>
2020-05-15 14:17:03 +0900

    push file

---
+++ algo_self/src/com/algo/self_200515/bj_1934.java
@@ -0,0 +1,39 @@
+package com.algo.self_200515;
+
+import java.io.*;
+import java.util.*;
+
+public class bj_1934 {
+	static int N, M,GCD,GMP;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(br.readLine(), " ");
+		int T = Integer.parseInt(st.nextToken());
+		for(int t=1;t<=T;t++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			gcd(N,M);
+			GMP=(N*M)/GCD;
+			bw.write(GMP+"\n");
+			
+		}	
+		bw.flush();
+		bw.close();
+	}
+
+	private static void gcd(int a, int b) {
+		if(b==0) {
+			GCD = a;
+			return;
+		}else {
+			int K = a%b;
+			gcd(b,K);
+		}
+		
+	}
+}
--- algo_self/src/com/algo/self_200515/bj_9613.java
+++ algo_self/src/com/algo/self_200515/bj_9613.java
@@ -3,23 +3,34 @@ package com.algo.self_200515;
 import java.io.*;
 import java.util.*;
 
-public class bj1934 {
-	static int N, M,GCD,GMP;
-
+public class bj_9613 {
+	static int[] array;
+	static long result;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = null;
-
+		
+		// 테스트캐이스 횟수
 		st = new StringTokenizer(br.readLine(), " ");
 		int T = Integer.parseInt(st.nextToken());
 		for(int t=1;t<=T;t++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			gcd(N,M);
-			GMP=(N*M)/GCD;
-			bw.write(GMP+"\n");
+			int testcase = Integer.parseInt(st.nextToken());
+			array = new int[101];
+			// 세부적인 테스트캐이스 횟수
+				for(int s=0;s<testcase;s++) {
+					array[s]=Integer.parseInt(st.nextToken());
+				}
+					for(int i=0;i<testcase-1;i++) {
+						if(i+1<testcase) {
+							for(int j=i+1;j<testcase;j++) {
+								gcd(array[i],array[j]);
+							}
+						}else continue;		
+					}
+				bw.write(result+"\n");
+				result=0;
 			
 		}	
 		bw.flush();
@@ -28,7 +39,10 @@ public class bj1934 {
 
 	private static void gcd(int a, int b) {
 		if(b==0) {
-			GCD = a;
+			result+= a;
+			return;
+		}else if(a==0 && a==0){
+			result+=0;
 			return;
 		}else {
 			int K = a%b;
