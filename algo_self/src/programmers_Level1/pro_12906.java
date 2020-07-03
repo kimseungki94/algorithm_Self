34894ac766677ab466e4d4aa53c3535dac8a0f82
김승기 <skkim0832@naver.com>
2020-07-03 13:16:35 +0900

    push

---
+++ algo_self/src/programmers_Level1/pro_12906.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.awt.*;
+public class pro_12906 {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        int judge=0;
+        int index=0;
+        
+        
+        for(int i=1;i<arr.length;i++){
+            if(arr[judge]==arr[i]){
+                arr[i]=-1;
+            }else{
+                judge=i;
+                index++;
+                continue;
+            }    
+        }
+        int length=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==-1){
+                continue;
+            }
+            else{
+                length++;
+            }
+        }
+        answer = new int[length];
+        int startindex=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==-1){
+                continue;
+            }else{
+                answer[startindex]=arr[i];
+                startindex++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
